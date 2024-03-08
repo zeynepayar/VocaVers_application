c7deeb66d546f343ab06834bbde9d9ee0567687c
Sude Suvar <sudesuvar51@gmail.com>
2024-03-08 22:32:16 +0300

    CHANGES WERE MADE IN THE DESIGN.

---
+++ app/src/main/java/com/sudedenizsuvar/vocaverseapplication/login_page.kt
@@ -0,0 +1,13 @@
+package com.sudedenizsuvar.vocaverseapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login_page : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+    }
+}
---
+++ app/src/main/java/com/sudedenizsuvar/vocaverseapplication/new_user_page.kt
@@ -0,0 +1,11 @@
+package com.sudedenizsuvar.vocaverseapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class new_user_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_user_page)
+    }
+}
