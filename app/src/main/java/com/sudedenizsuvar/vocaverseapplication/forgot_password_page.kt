83c85f032f1a2a12b07703b8dbf077871c0a9dcc
Sude Suvar <sudesuvar51@gmail.com>
2024-03-09 23:00:35 +0300

    CHANGES WERE MADE IN THE DESIGN.

---
+++ app/src/main/java/com/sudedenizsuvar/vocaverseapplication/forgot_password_page.kt
@@ -0,0 +1,11 @@
+package com.sudedenizsuvar.vocaverseapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class forgot_password_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.forgot_password_page)
+    }
+}
